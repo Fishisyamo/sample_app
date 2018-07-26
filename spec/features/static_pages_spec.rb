7d4f1b6a4b56115fbcdbb5069b28a09b4b0df6fd
Fishisyamo <cokorono@gmail.com>
2018-07-26 13:54:06 +0900

    Add rails-flavored-ruby

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,18 +1,25 @@
 require 'rails_helper'
 
 RSpec.feature "StaticPages", type: :feature do
-  scenario '#home have title' do
-    visit root_url
-    expect(page).to have_title 'Home'
+  feature '#home' do
+    scenario '#home not have page title' do
+      visit root_path
+      expect(page).to_not have_title 'Home | '
+    end
+
+    scenario '#home have full_title only' do
+      visit root_path
+      expect(page).to have_title 'Ruby on Rails Tutorial Sample App'
+    end
   end
 
   scenario '#help have title' do
-    visit static_pages_help_url
+    visit static_pages_help_path
     expect(page).to have_title 'Help'
   end
 
   scenario '#about have title' do
-    visit static_pages_about_url
+    visit static_pages_about_path
     expect(page).to have_title 'About'
   end
 
